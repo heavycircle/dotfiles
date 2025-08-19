6c0659fd181db129e661457c247f56fe7478a5f9
heavycircle <bleeper_genius0c@icloud.com>
2025-08-19 15:45:23 -0400

    nvim oil

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -1,8 +1,12 @@
 vim.pack.add({
     { src = 'https://github.com/NvChad/showkeys', opt = true },
+    { src = 'https://github.com/stevearc/oil.nvim' },
 })
 
+require "oil".setup()
 require "showkeys".setup({ position = "top-right" })
+
+vim.keymap.set("n", "<leader>e", "<cmd>Oil<CR>", help="Oil")
 
 -- Colorscheme
 require "plugins.colorscheme"
--- config/nvim/lua/plugins/snippets.lua
+++ config/nvim/lua/plugins/snippets.lua
@@ -10,5 +10,5 @@ require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets/" })
 -- Define snippets
 local ls = require("luasnip")
 vim.keymap.set("i", "<C-e>", function() ls.expand_or_jump(1) end, { silent = true })
--- vim.keymap.set({ "i", "s" }, "<C-J>", function() ls.jump(1) end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-J>", function() ls.jump(1) end, { silent = true })
 vim.keymap.set({ "i", "s" }, "<C-K>", function() ls.jump(-1) end, { silent = true })
